988a60f8f05950b7c0ec2e42bd1d1e29bb0b1c24
LorenzoBianchi02 <bianchi_lorenzo@hotmail.com>
2024-09-04 13:50:40 +0200

    testing

--- Fianco.c
+++ Fianco.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int negaMax(int board[9][9], int depth, int alfa, int beta);
 
+int main(){
+    int board[9][9];
+    printf("%d\n", negaMax(board, 1, -1000, 1000));
+}
+
+int negaMax(int board[9][9], int depth, int alfa, int beta){
     
 }
---
+++ src/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    __uint128_t tmp = 5;
+    printf("ok %d\n");
+}
